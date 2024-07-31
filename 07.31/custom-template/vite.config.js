1a0dc55d8b2d09adb81b253df317eb93100d6f1b
ksh17 <rkstkd98@naver.com>
2024-07-31 14:10:38 +0900

    각각 src 경로 @으로 교체

--- 07.31/custom-template/vite.config.js
+++ 07.31/custom-template/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import pluginReact from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@/": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   plugins: [
     pluginReact({
       jsxRuntime: "automatic",
